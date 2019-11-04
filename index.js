346bb52161ae8a0ed42f3b4f4d00eac92bbf2f64
ahyunlee5 <ahyunlee@umich.edu>
2019-11-03 19:25:30 -0800

    update 1

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 'use strict';
+
+function formatQueryParams(params) {
+  const queryItems = Object.keys(params)
+    .map(key => `${key} = ${params[key]}`);
+  return queryItems.join('&');
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  $('#results-list').empty();
+  for (let i = 0; i < responseJson.message; i++) {
+    console.log('display result');
+    $('#results-list').append(
+      `<p><a href="${url} target="_blank"">${repo_name}</a></p>`
+    );
+    $('#results').removeClass('hidden');
+  }
+}
+
+function getRepos(userInput) {
+  fetch(`https://api.github.com/users/${userInput}/repos`)
+    .then(response => {
+      if (response.ok) {
+        console.log('repsonse okay');
+        return response.json();
+      }
+      throw new Error (response.statusText);
+    })
+    .then(responseJson => displayResults(responseJson))
+    .catch(error => {
+      $('#js-error-message').text(`Something went wrong: ${error.message}`);
+    });
+}
+
+
+function watchForm () {
+  $('form').on('submit', function() {
+    event.preventDefault();
+    let userInput = $('#js-search-term').val();
+    $('#results').empty();
+    getRepos();
+  });
+}
+$(function() {
+  console.log('App loaded. Waiting for user input');
+  watchForm();
+})
